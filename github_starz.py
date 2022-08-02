0d5a5c62f1c6bfe60eb69cd8fe8c70d35cb98b34
Thomas Vincent <tlfvincent@gmail.com>
2022-08-01 22:34:45 -0400

    remove url extraction function

--- github_starz.py
+++ github_starz.py
@@ -12,9 +12,8 @@ statistics collected are:
 """
 
 import markdown
-import ssl
-import urllib.request
 import re
+
 from github import Github
 
 import pandas as pd
@@ -23,32 +22,6 @@ _URL = "https://github.com/tlfvincent/ml-resources/blob/main/README.md"
 
 # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 _GITHUB_TOKEN = "your_github_token"
-
-
-def extract_data(url):
-    """
-    Extract raw HTML data from specified URL.
-
-    Parameters
-    ----------
-    url : str
-        URL parameter to extract raw data from.
-
-    Returns
-    -------
-    data : str
-        Raw html data extracted from URL.
-    """
-
-    # we will opt out of certificate verification on a single connection
-    # see `https://peps.python.org/pep-0476/` for more details
-    context = ssl._create_unverified_context()
-
-    req = urllib.request.Request(url)
-    resp = urllib.request.urlopen(req, context=context)
-    html_data = resp.read()
-
-    return html_data
 
 
 def extract_links(data):
@@ -123,9 +96,11 @@ if __name__ == "__main__":
     """
     This is the entrypoint for the module.
     """
-    html_data = extract_data(_URL)
 
-    github_repo_links = extract_links(html_data)
+    with open("README.md", "r") as f:
+        readme = f.read()
+
+    github_repo_links = extract_links(readme)
 
     github_stats = extract_github_repo_statistics(github_repo_links)
 
